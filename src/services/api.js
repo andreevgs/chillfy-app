ac3664efef4e8fd39563d7f27a373c84772f4468
AndreevG <andreev.gs12@yandex.by>
2022-09-11 01:34:18 +0300

    change api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: "https://chillfy.herokuapp.com/api/",
-    baseURL: "http://localhost:3000/api/",
+    baseURL: "https://chillfy.herokuapp.com/api/",
+    // baseURL: "http://localhost:3000/api/",
     headers: {
         "Content-Type": "application/json",
     },
